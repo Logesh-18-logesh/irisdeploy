70e1275832cb03f7d4ca07d5ed20bf97afa2027a
LOGESH SD <148134953+Logesh-18-logesh@users.noreply.github.com>
2024-06-27 17:43:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 import os
-
+import numpy
 # Load dataset from CSV file
 data = pd.read_csv('Iris.csv')
 
